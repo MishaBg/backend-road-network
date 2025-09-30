6d97b43bf6fb8d75ff19fb5879100d1503adcc52
MishaBg <151045905+MishaBg@users.noreply.github.com>
2025-09-30 06:26:32 +0300

    Create handler.go

---
+++ internal/app/handler/handler.go
@@ -0,0 +1,42 @@
+package handler
+
+import (
+	"lab1-design-backend/internal/app/repository"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type Handler struct {
+	Repository *repository.Repository
+}
+
+func NewHandler(r *repository.Repository) *Handler {
+	return &Handler{
+		Repository: r,
+	}
+}
+
+// RegisterHandler Функция, в которой мы отдельно регистрируем маршруты, чтобы не писать все в одном месте
+func (h *Handler) RegisterHandler(router *gin.Engine) {
+	router.GET("/cites", h.GetCites)
+	router.GET("/city/:id", h.GetCity)
+	router.GET("/service/:id", h.ServiceHandler)
+	router.POST("/cites/:id/add-to-service", h.AddCityToService)
+	router.POST("/service/:id/delete", h.DeleteService)
+}
+
+// RegisterStatic То же самое, что и с маршрутами, регистрируем статику
+func (h *Handler) RegisterStatic(router *gin.Engine) {
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/styles", "/Users/mihailbagin/Downloads/lab1-design-backend/resources/styles")
+}
+
+// errorHandler для более удобного вывода ошибок
+func (h *Handler) errorHandler(ctx *gin.Context, errorStatusCode int, err error) {
+	logrus.Error(err.Error())
+	ctx.JSON(errorStatusCode, gin.H{
+		"status":      "error",
+		"description": err.Error(),
+	})
+}
